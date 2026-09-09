06bc0aed13f35efd9efeb6b1ab76082a642526c0
pope399 <joseph.rodriguez.picado@est.una.ac.cr>
2026-09-08 21:10:16 -0600

    añadir Model y completar patrón MVC en Funcionario

--- src/main/java/sistema/reservas/Logic/Funcionario.java
+++ src/main/java/sistema/reservas/Logic/Funcionario.java
@@ -1,8 +1,17 @@
 package sistema.reservas.Logic;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Funcionario extends Usuario {
 
     private String telefono;
+
+    /** JAXB necesita un constructor vacio para poder des-serializar. */
+    public Funcionario() {
+        super();
+    }
 
     public Funcionario(int id, String nombre, String username, String password, String telefono) {
         super(id, nombre, username, password);
